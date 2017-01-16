831de81cf9d00bd96094ca63e85fa9d05d54f89c
Renier Morales <renierm@us.ibm.com>
2017-01-16 19:19:09 -0400

    Handle new branches for gets as well

--- in.sh
+++ in.sh
@@ -43,11 +43,13 @@ if [ "${deleted}" == "null" ] || [ "${deleted}" == "true" ]; then
     exit 0
 fi
 
+created=$(jq -r '.created // ""' < $event)
 branch=$(cat $event | jq -r '.ref' | sed -e 's/refs\/heads\///')
 [[ "$branch" == *"/tags/"* ]] && TAG=1
 branch=$(echo $branch | sed -e 's/refs\/tags\///')
 url=$(jq -r '.repository.clone_url // ""' < $event)
 before=$(jq -r '.before // ""' < $event)
+[ "$created" == "true" ] && [ "$before" == "$ZERO" ] && TAG=1
 after=$(jq -r '.after' < $event)
 [ -n "$TAG" ] && after="$branch"
 
@@ -64,8 +66,8 @@ if [ -z "${before}" ] || [ "${before}" == "${ZERO}" ]; then
 fi
 
 range="${before}..${after}"
-if [ "${before}" == "${ZERO}" ] || [ -z "${before}" ]; then
-    range="${after}"
+if [ "${before}" == "${ZERO}" ] || [ -z "${before}" ] || [ "$before" == "$after"]; then
+    range="${after}~1..${after}"
 fi
 
 # destination directory as $1
